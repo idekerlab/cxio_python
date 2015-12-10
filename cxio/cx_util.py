4f0434285042753c687b088f68a7548c701457e6
cmzmasek <cmzmasek@yahoo.com>
2015-12-10 12:09:39 -0800

    added "create" static helper methods, corresponding to cx_helper.py from ndex

--- cxio/cx_util.py
+++ cxio/cx_util.py
@@ -37,6 +37,63 @@ class CxUtil(object):
             e = {'@id': node_id, 'n': node_name, 'r': node_represents}
         return AspectElement(CxConstants.NODES, e)
 
+    @staticmethod
+    def create_edges_aspect_element(edge_id, source_id, target_id, interaction):
+        """
+        :rtype: AspectElement
+        """
+        e = {'@id': edge_id,
+             's': source_id,
+             't': target_id,
+             'i': interaction
+             }
+        return AspectElement(CxConstants.EDGES, e)
+
+    @staticmethod
+    def create_edge_attribute_aspect_element(edge_id, name, value):
+        """
+        :rtype: AspectElement
+        """
+        e = {'po': edge_id,
+             'n': name,
+             'v': value
+             }
+        return AspectElement(CxConstants.EDGE_ATTRIBUTES, e)
+
+    @staticmethod
+    def create_node_attribute_aspect_element(node_id, name, value, att_type=None):
+        """
+        :rtype: AspectElement
+        """
+        e = {'po': node_id,
+             'n': name,
+             'v': value
+             }
+        if att_type:
+            e['t'] = att_type
+        return AspectElement(CxConstants.NODE_ATTRIBUTES, e)
+
+    @staticmethod
+    def create_pre_metadata_element(aspect_name, consistency_group, version, last_update):
+        """
+        :rtype: Element
+        """
+        e = {'name': aspect_name,
+             'consistencyGroup': consistency_group,
+             'version': version,
+             'lastUpdate': last_update
+             }
+        return Element('metaData', e)
+
+    @staticmethod
+    def create_post_metadata_element(aspect_name, id_counter):
+        """
+        :rtype: Element
+        """
+        e = {'name': aspect_name,
+             'idCounter': id_counter,
+             }
+        return Element('metaData', e)
 
     @staticmethod
     def create_number_verification_element():
@@ -45,3 +102,76 @@ class CxUtil(object):
         """
         e = [dict(longNumber=CxConstants.NUMBER_VERIFICATION_VALUE)]
         return Element(CxConstants.NUMBER_VERIFICATION, e)
+
+    @staticmethod
+    def create_ndex_citation_aspect_element(citation_id, citation_type, title, contributors, identifier, description):
+        """
+        :rtype: AspectElement
+        """
+        e = {'@id': citation_id,
+             'dc:title': title,
+             'dc:contributor': contributors,
+             'dc:identifier': identifier,
+             'dc:type': citation_type,
+             'dc:description': description,
+             'attributes': []
+             }
+        return AspectElement('citations', e)
+
+    @staticmethod
+    def create_ndex_support_aspect_element(support_id, cx_citation_id, text):
+        """
+        :rtype: AspectElement
+        """
+        e = {'@id': support_id,
+             'citation': cx_citation_id,
+             'text': text,
+             'attributes': []
+             }
+        return AspectElement('supports', e)
+
+    @staticmethod
+    def create_ndex_function_term_aspect_element(function_term):
+        """
+        :rtype: AspectElement
+        """
+        e = {'function_term': function_term}
+        return AspectElement('functionTerms', e)
+
+    @staticmethod
+    def create_ndex_node_citation_aspect_element(node_id, citation_id):
+        """
+        :rtype: AspectElement
+        """
+        e = {'citations': [citation_id],
+             'po': [node_id]
+             }
+        return AspectElement('nodeCitations', e)
+
+    @staticmethod
+    def create_ndex_node_support_aspect_element(node_id, support_id):
+        """
+        :rtype: AspectElement
+        """
+        e = {'supports': [support_id],
+             'po': [node_id]
+             }
+        return AspectElement('nodeSupports', e)
+
+    @staticmethod
+    def create_ndex_edge_support_aspect_element(edge_id, support_id):
+        """
+        :rtype: AspectElement
+        """
+        e = {'supports': [support_id],
+             'po': [edge_id]
+             }
+        return AspectElement('edgeSupports', e)
+
+    @staticmethod
+    def create_ndex_context_element(contexts):
+        """
+        :rtype: Element
+        """
+        return Element('@context', contexts)
+
