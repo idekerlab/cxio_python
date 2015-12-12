2f894aa29746bdb82413e2192e11b62adc8d6e84
cmzmasek <cmzmasek@yahoo.com>
2015-12-11 17:41:33 -0800

    more aspects,  basic error checks

--- test/test1.py
+++ test/test1.py
@@ -3,7 +3,7 @@ from cxio.cx_reader import CxReader
 from cxio.cx_writer import CxWriter
 from cxio.aspect_element import AspectElement
 from cxio.cx_constants import CxConstants
-from cxio.cx_util import CxUtil
+from cxio.element_maker import ElementMaker
 
 import unittest
 
@@ -40,8 +40,21 @@ class MyTestCase(unittest.TestCase):
         #self.assertEqual()
 
     def test_2(self):
-        n = CxUtil.create_nodes_aspect_element(1, 'name', 'represents')
-        print(str(n))
+        n1 = ElementMaker.create_nodes_aspect_element(1, 'node 1', 'N1')
+        n2 = ElementMaker.create_nodes_aspect_element(2, 'node 2', 'N2')
+        e = ElementMaker.create_edges_aspect_element(3, 1, 2, '1->2')
+        s = ElementMaker.create_status_element(True, "msg")
+        nea = ElementMaker.create_network_attributes_aspect_element(1200, 'size', 12.3, CxConstants.DATA_TYPE_DOUBLE)
+        noa = ElementMaker.create_node_attributes_aspect_element(1200, 1, 'weight', '12.0', CxConstants.DATA_TYPE_FLOAT)
+        eda = ElementMaker.create_edge_attributes_aspect_element(1200, 3, 'length', 303.409883, CxConstants.DATA_TYPE_DOUBLE)
+
+        print(str(n1))
+        print(str(n2))
+        print(str(e))
+        print(str(s))
+        print(str(nea))
+        print(str(noa))
+        print(str(eda))
 
 if __name__ == '__main__':
     unittest.main()
